c596c905659e23a30b7a5a7228364d35aadcb9b8
shariffDevMern <sheriffgaming926@gmail.com>
2025-01-12 09:12:05 +0530

    slight updates

--- src/app/ components/Greet.tsx
+++
@@ -1,1 +0,0 @@
-export const Greet = () => <h1>Hell</h1>;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,3 @@
-import {
-  Avatar,
-  Box,
-  Button,
-  Container,
-  Grid,
-  Typography,
-} from "@mui/material";
-import ScienceIcon from "@mui/icons-material/Science";
-import EnergySavingsLeafIcon from "@mui/icons-material/EnergySavingsLeaf";
-import { FlagCircleOutlined } from "@mui/icons-material";
 import { HeroSection } from "./ components/HeroSection";
 
 export default function Home() {
